6ad25a171b1a8690604d3f6fa25588e9450796dc
Simon C. Wu <simon.c.wu@gmail.com>
2018-01-10 21:11:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ function happyHolidayTo(holiday, name) {
 }
 
 function holidayCountdown(holiday, days) {
-  `It's ${days} days until ${holiday}!`
+  return `It's ${days} days until ${holiday}!`
 }
